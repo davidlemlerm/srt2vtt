5904fba7f1b2a22f7a56e4870df753fec67d4b71
David Lemler <dlemler@pm.me>
2020-11-24 00:26:15 -0600

    Make the program a lot better

--- srt2vtt.py
+++ srt2vtt.py
@@ -1,19 +1,64 @@
+# Imports
 import os.path
-print("Please enter the name of the file to open:", end=" ")
+import sys
+import getopt
+
+# Variables
+inputfilename = None
+outputfilename = None
+
+# Argument processing
+args, params = getopt.getopt(sys.argv[1:], "i:o:")
+for arg, param in args:
+	if arg == "-i":
+		inputfilename = param
+	elif arg == "-o":
+		outputfilename = param
+
+# If input file is not specified in arguments, request it from the user
+if inputfilename == None:
+	print("Please enter the name of the file to open:", end=" ")
+	inputfilename = input()
 done = False
 while done == False:
-	if os.path.isfile(filename):
+	if os.path.isfile(inputfilename):
 		done = True
 	else:
 		print("File not found, please enter the filename:", end=" ")
-input = open(filename)
-print("WEBVTT")
-print()
-for line in input:
-	if "-->" in line:
-		# We need to perform a substitution.
-		newline = line.replace(",", ".")
-		print(newline, end="")
-	else:
-		# We don't need to do anything special.
-		print(line, end="")
+		inputfilename = input()
+
+# Open the file
+inputfile = open(inputfilename)
+
+# If we're outputting to the console
+if outputfilename == None:
+	print("WEBVTT")
+	print()
+	for line in inputfile:
+		if "-->" in line:
+			newline = line.replace(",", ".")
+			print(newline, end="")
+		else:
+			print(line, end="")
+
+# If we're outputting to a file
+else:
+	if os.path.isfile(outputfilename):
+		print("The file \"{0}\" already exists. Overwrite?:".format(outputfilename), end=" ")
+		if input().lower() != "true":
+			print("Exiting.")
+			exit()
+	output = open(outputfilename, "w")
+	output.write("WEBVTT")
+	output.write("\n\n")
+	for line in inputfile:
+		if "-->" in line:
+			newline = line.replace(",", ".")
+			output.write(newline)
+		else:
+			output.write(line)
+	# Close our output file
+	output.close()
+			
+# Close our input file
+inputfile.close()
